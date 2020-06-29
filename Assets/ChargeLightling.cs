0d7bd09e9e828cfc2896099fac2aeaa3abba325a
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-06-29 12:32:58 -0700

    added timer

--- Assets/ChargeLightling.cs
+++ Assets/ChargeLightling.cs
@@ -1,27 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ChargeLightling : MonoBehaviour
 {
     public GameObject Lightning;
     public Collider collider;
     public AudioSource boom;
+    private PhotonView PV;
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             other.GetComponent<PlayerController>().Camera.GetComponent<CameraFollow>().LightningAnim.SetTrigger("Shake");
             other.GetComponent<PlayerController>().Camera.GetComponent<CameraFollow>().lightningScript.MoveUp();
-            Lightning.SetActive(false);
-            collider.enabled = false;
-            StartCoroutine(ChargeUp());
+            PV.RPC("Disable", RpcTarget.MasterClient);
+            
         }
     }
 
-    IEnumerator ChargeUp()
+    [PunRPC]
+    private void Disable()
     {
-        yield return new WaitForSeconds(Random.Range(15, 40));
+        int t = Random.Range(15, 40);
+        PV.RPC("Recharge", RpcTarget.All, t);
+    }
+    
+    [PunRPC]
+    void Recharge(int t)
+    {
+        Lightning.SetActive(false);
+        collider.enabled = false;
+        StartCoroutine(ChargeUp(t));
+    }
+
+
+    IEnumerator ChargeUp(int time)
+    {
+        yield return new WaitForSeconds(time);
         Lightning.SetActive(true);
         collider.enabled = true;
         boom.enabled = true;
---
+++ Assets/TimerControler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class TimerControler : MonoBehaviour
+{
+    private PhotonView PV;
+    private Text textUI;
+    private double startTIme;
+    public float time = 120;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        textUI = GetComponent<Text>();
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PV.RPC("starTimer", RpcTarget.All, PhotonNetwork.Time);
+        }
+    }
+
+    [PunRPC]
+    void starTimer(double time)
+    {
+        startTIme = time;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(startTIme + 1 <= PhotonNetwork.Time)
+        {
+            time -= Time.deltaTime;
+            textUI.text = decimal.Truncate(decimal.Parse(time.ToString())).ToString();
+        }
+    }
+}
