226c35337e4847b31a5235a2ddd5fa8242f51ddd
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-06-29 11:39:28 -0700

    materials

--- Assets/ChargeLightling.cs
+++ Assets/ChargeLightling.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ChargeLightling : MonoBehaviour
 {
+    public GameObject Lightning;
+    public Collider collider;
+    public AudioSource boom;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             other.GetComponent<PlayerController>().Camera.GetComponent<CameraFollow>().LightningAnim.SetTrigger("Shake");
             other.GetComponent<PlayerController>().Camera.GetComponent<CameraFollow>().lightningScript.MoveUp();
-            gameObject.SetActive(false);
+            Lightning.SetActive(false);
+            collider.enabled = false;
+            StartCoroutine(ChargeUp());
         }
+    }
+
+    IEnumerator ChargeUp()
+    {
+        yield return new WaitForSeconds(Random.Range(15, 40));
+        Lightning.SetActive(true);
+        collider.enabled = true;
+        boom.enabled = true;
     }
 }
