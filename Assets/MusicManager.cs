092c39e81915d26b30500a4d5a617a01ad823e02
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-06-30 17:17:19 -0700

    music fix

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
@@ -28,6 +29,9 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
