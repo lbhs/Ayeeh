b3875273faee63590e27ab07585fe97dbf94029b
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-06 16:01:02 -0700

    added an option to restart and new image

---
+++ Assets/PlayAgainScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class PlayAgainScript : MonoBehaviour
+{
+    public int menuSceneIndex=0;
+    public void PlayAgainButton()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene(menuSceneIndex);
+    }
+}
