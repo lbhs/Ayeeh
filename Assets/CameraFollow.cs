fc87f458616cbbba374773b29ff4163be27f5ef7
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-06 23:37:51 -0700

    Update CameraFollow.cs

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -39,8 +39,8 @@ public class CameraFollow : MonoBehaviour {
         transform.rotation = CameraFollowObj.transform.rotation;
         if (mobileSupport)
         {
-            yaw += 1.75f * RJ.GetInputDirection().x;
-            pitch -= 1f * RJ.GetInputDirection().y;
+            yaw += 1.75f * RJ.GetInputDirection().x * Time.deltaTime;
+            pitch -= 1f * RJ.GetInputDirection().y * Time.deltaTime;
         }
         else
         {
